6197fa20ba33579754c314591280051467c229b2
mbrsagor <mbrsagor@gmail.com>
2021-12-05 13:08:58 +0600

    New: face detection working

---
+++ detection/maks.py
@@ -0,0 +1,28 @@
+import os
+import time
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+face_detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+count = 0
+
+while True:
+    ret, frame = video.read()
+    faces = face_detect.detectMultiScale(frame, 1.3, 5)
+    for x, y, w, h in faces:
+        count = count + 1
+        name = './images/face_without_mask/' + str(count) + '.jpg'
+        print(f"Creating images.............. {name}")
+        cv2.imwrite(name, frame[y:y + h, x:x+w])
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1)
+
+    # if k == ord('q'):
+    if count > 50:
+        break
+
+video.release()
+cv2.destroyAllWindows()
