c1ebbed5b3b3a3d5343f87cf8693fb1301b43b5f
mbrsagor <mbrsagor@gmail.com>
2021-12-05 19:37:31 +0600

    Refactor: The code convert class based

--- detection/maks.py
+++ detection/maks.py
@@ -1,28 +1,34 @@
-import os
-import time
 import cv2
-import numpy as np
 
-video = cv2.VideoCapture(0)
-face_detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-count = 0
+class FaceDetection(object):
 
-while True:
-    ret, frame = video.read()
-    faces = face_detect.detectMultiScale(frame, 1.3, 5)
-    for x, y, w, h in faces:
-        count = count + 1
-        name = './images/face_without_mask/' + str(count) + '.jpg'
-        print(f"Creating images.............. {name}")
-        cv2.imwrite(name, frame[y:y + h, x:x+w])
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1)
+    def __init__(self):
+        self.count = 0
 
-    # if k == ord('q'):
-    if count > 50:
-        break
+    def face_detection(self):
+        video = cv2.VideoCapture(0)
+        face_detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-video.release()
-cv2.destroyAllWindows()
+        while True:
+            ret, frame = video.read()
+            faces = face_detect.detectMultiScale(frame, 1.3, 5)
+            for x, y, w, h in faces:
+                count = self.count + 1
+                name = './images/face_without_mask/' + str(count) + '.jpg'
+                print(f"Creating images.............. {name}")
+                cv2.imwrite(name, frame[y:y + h, x:x + w])
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.imshow('frame', frame)
+            k = cv2.waitKey(1)
+
+            # if k == ord('q'):
+            if self.count > 50:
+                break
+
+        video.release()
+        cv2.destroyAllWindows()
+
+
+face_dec = FaceDetection()
+face_dec.face_detection()
