591a1c24b2bd4269d725b07066f36aec2648c10d
mbrsagor <mbrsagor@gmail.com>
2023-10-05 21:05:45 +0600

    Update: Opencv Python program for Face detection update

--- detection/fd.py
+++ detection/fd.py
@@ -1,57 +1,35 @@
 import cv2
+import sys
 
-# load the required trained XML classifiers
-# https://github.com/Itseez/opencv/blob/master/
-# data/haarcascades/haarcascade_frontalface_default.xml
-# Trained XML classifiers describes some features of some
-# object we want to detect a cascade function is trained
-# from a lot of positive(faces) and negative(non-faces)
-# images.
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-# https://github.com/Itseez/opencv/blob/master
-# /data/haarcascades/haarcascade_eye.xml
-# Trained XML file for detecting eyes
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+video_capture = cv2.VideoCapture(0)
 
-# capture frames from a camera
-cap = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
 
-# loop runs if capturing has been initialized.
-while 1:
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # reads frames from a camera
-    ret, img = cap.read()
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
-    # convert to gray scale of each frames
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detects faces of different sizes in the input image
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
+    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        # To draw a rectangle in a face
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Detects eyes of different sizes in the input image
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
 
-        # To draw a rectangle in eyes
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
-
-            # Display an image in a window
-    cv2.imshow('img', img)
-
-    # Wait for Esc key to stop
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Close the window
-cap.release()
-
-# De-allocate any associated memory usage
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
